ae751b8636d60b9a757a08c9f2b8651f5a3b447a
Yaasir Cheekoory <yaasir1997@gmail.com>
2023-01-12 20:40:02 +0400

    model, routes completed

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "gardens#index"
+  resources :gardens do
+    resources :plants
+  end
 end
