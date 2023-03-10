2c85b453e4244baf33c13b565b922e2b26c91c03
Yaasir Cheekoory <yaasir1997@gmail.com>
2023-03-11 01:41:49 +0400

    more to go

---
+++ app/controllers/gardens_controller.rb
@@ -0,0 +1,11 @@
+class GardensController < ApplicationController
+
+  def index
+    @gardens = Garden.all
+  end
+
+  def show
+    @garden = Garden.find(params[:id])
+    @plants = Plant.all
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts 'Cleaning database...'
+Garden.destroy_all
+Plant.destroy_all
+puts 'Creating restaurants..'
+25.times do
+  Garden.create(
+    name: Faker::Restaurant.name
+  )
+end
+
+25.times do
+  Plant.create(
+    name: Faker::Restaurant.name,
+    image_url: Faker::Book.title
+  )
+end
+puts 'Finished!'
